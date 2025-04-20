2762c32ef0bc8f0f2f9aa772cfbe4f91b4581f11
Deepaksingh <itsdeepaksingh00@gmail.com>
2025-04-20 19:44:35 +0530

    requirements & Installation done

--- setup.py
+++ setup.py
@@ -14,15 +14,22 @@ AUTHOR_NAME = "Deepak Singh"
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author=AUTHOR_USER_NAME,
+    author=AUTHOR_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for nlp app",
+    description="A small python package for NLP-based text summarization app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },  # ← comma added here
+    },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # or your license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],  # You can leave it empty or populate if you want pip install to auto-install deps
 )
